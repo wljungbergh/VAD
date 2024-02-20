74050f93ff2d77ff120eb7e5f2409e0706ce0957
William Ljungbergh <william.ljungbergh@zenseact.com>
2024-02-20 08:51:34 +0100

    Add inference config

---
+++ projects/configs/VAD/VAD_inference.py
@@ -0,0 +1,10 @@
+_base_ = ["../VAD_base_e2e.py"]
+
+# we have to change this according to their docs
+# https://github.com/wljungbergh/VAD/blob/main/docs/train_eval.md
+img_norm_cfg = dict(mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
+
+inference_pipeline = [
+    dict(type="NormalizeMultiviewImage", **img_norm_cfg),
+    dict(type="PadMultiViewImage", size_divisor=32),
+]
