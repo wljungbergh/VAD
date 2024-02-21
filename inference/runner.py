e4d8525011e60575a9d013e6a3f372c38923d447
William Ljungbergh <william.ljungbergh@zenseact.com>
2024-02-21 13:01:47 +0100

    Fully working runner

--- inference/runner.py
+++ inference/runner.py
@@ -13,50 +13,10 @@ import numpy as np
 from pyquaternion import Quaternion
 import torch
 
-from mmdet3d.datasets import NuScenesDataset
 from projects.mmdet3d_plugin.VAD.VAD import VAD
 from nuscenes.eval.common.utils import (
     quaternion_yaw,
 )
-
-NUSCENES_CAM_ORDER = [
-    "CAM_FRONT",
-    "CAM_FRONT_RIGHT",
-    "CAM_FRONT_LEFT",
-    "CAM_BACK",
-    "CAM_BACK_LEFT",
-    "CAM_BACK_RIGHT",
-]
-
-# NOTE: this is what they do to the can bus signals
-# in preproc of the dataset
-### pos 3 | m in global frame
-### orientation 4 | quaternion expressed in the ego-vehicle frame
-### accel 3 | m/s^2 in ego-vehicle frame
-### rotation_rate 3 | rad/s in ego-vehicle frame
-### vel 3 | m/s in ego-vehicle frame
-### zeros 2
-
-# in preproc of the dataloader
-### rotation = Quaternion(input_dict["ego2global_rotation"]) # = nuscenes pose_record["rotation"]
-### translation = input_dict["ego2global_translation"] # = nuscenes pose_record["translation"]
-### can_bus = input_dict["can_bus"]
-### can_bus[:3] = translation
-### can_bus[3:7] = rotation
-### patch_angle = quaternion_yaw(rotation) / np.pi * 180
-### if patch_angle < 0:
-###     patch_angle += 360
-### can_bus[-2] = patch_angle / 180 * np.pi
-### can_bus[-1] = patch_angle
-
-# this means that the can bus signals are:
-# 0-3 translation in global frame (to ego-frame)
-# 3-7 rotation in global frame (to ego-frame)
-# 7-10 acceleration in ego-frame
-# 10-13 rotation rate in ego-frame
-# 13-16 velocity in ego-frame
-# 16 patch angle in degrees
-# 17 patch angle in radians
 
 
 @dataclass
@@ -81,9 +41,9 @@ class VADAuxOutputs:
     object_classes: Optional[List[str]] = None  # (N, )
     object_scores: Optional[List[float]] = None  # (N, )
     segmentation: Optional[List[List[float]]] = None
-    seg_grid_centers: Optional[List[List[List[float]]]] = (
-        None  # bev_h (200), bev_w (200), 2 (x & y)
-    )
+    seg_grid_centers: Optional[
+        List[List[List[float]]]
+    ] = None  # bev_h (200), bev_w (200), 2 (x & y)
 
     def to_json(self) -> dict:
         return dict(
@@ -159,7 +119,7 @@ class VADRunner:
         # permute rgb -> bgr
         imgs = input.imgs[:, :, :, ::-1]
         # input to preproc shoudl be dict(img=imgs) where imgs: n x h x w x c in bgr format
-        preproc_input = dict(img=imgs)
+        preproc_input = dict(img=imgs, lidar2img=input.lidar2img)
         # run it through the inference pipeline (which is same as eval pipeline except not loading annotations)
         preproc_output = self.preproc_pipeline(preproc_input)
         # collect in array as will convert to tensor, but currently it is a list of arrays (n, h, w, c)
@@ -170,16 +130,6 @@ class VADRunner:
         imgs = torch.from_numpy(imgs).to(self.device)
         # img should be (1, n, 3, h, w)
         imgs = imgs.unsqueeze(0)
-        # move other input to the device as well
-        l2g_t = (
-            torch.from_numpy(input.lidar_pose[:3, 3]).to(self.device).unsqueeze(0)
-        ).float()  # should be 1x3
-        l2g_r_mat = (
-            torch.from_numpy(input.lidar_pose[:3, :3]).to(self.device).unsqueeze(0)
-        ).float()  # should be 1x3x3
-        timestamp = (
-            torch.from_numpy(np.array([input.timestamp])).to(self.device).unsqueeze(0)
-        )
         # we are preproccessing the canbus signals only currently.
         # TODO: fix preproc to include the image preprocessing as well. this is currently done
         # in mmcv (i.e., numpy) and not torch.
@@ -190,9 +140,8 @@ class VADRunner:
             {
                 "scene_token": self.scene_token,
                 "can_bus": input.can_bus_signals,
-                "lidar2img": input.lidar2img,  # lidar2cam @ camera2img
+                "lidar2img": preproc_output["lidar2img"],  # lidar2cam @ camera2img
                 "img_shape": preproc_output["img_shape"],
-                # we need this as they are used in the model somewhere.
                 "box_type_3d": LiDARInstance3DBoxes,
             }
         ]
@@ -211,16 +160,18 @@ class VADRunner:
         self.prev_frame_info["prev_pos"] = tmp_pos
         self.prev_frame_info["prev_angle"] = tmp_angle
 
-        img_feats = self.extract_feat(img=imgs, img_metas=img_metas)
-
-        outs = self.pts_bbox_head(
+        img_feats = self.model.extract_feat(img=imgs, img_metas=img_metas)
+        outs = self.model.pts_bbox_head(
             img_feats,
             img_metas,
             prev_bev=self.prev_frame_info["prev_bev"],
-            ego_his_trajs=None,  # figure these out
-            ego_lcf_feat=None,  # figure these out
+            ego_his_trajs=None,  #  dont have to because the dont use the history trajs
+            ego_lcf_feat=None,  # these are not used either
         )
-        bbox_list = self.pts_bbox_head.get_bboxes(outs, img_metas, rescale=False)
+        # save the bev feature map
+        self.prev_frame_info["prev_bev"] = outs["bev_embed"]
+
+        bbox_list = self.model.pts_bbox_head.get_bboxes(outs, img_metas, rescale=False)
 
         bbox_results = []
         for i, (
@@ -235,16 +186,25 @@ class VADRunner:
         ) in enumerate(bbox_list):
             bbox_result = bbox3d2result(bboxes, scores, labels)
             bbox_result["trajs_3d"] = trajs.cpu()
-            map_bbox_result = self.map_pred2result(
+            map_bbox_result = self.model.map_pred2result(
                 map_bboxes, map_scores, map_labels, map_pts
             )
             bbox_result.update(map_bbox_result)
             bbox_result["ego_fut_preds"] = outs["ego_fut_preds"][i].cpu()
-            bbox_result["ego_fut_cmd"] = None  # ego_fut_cmd.cpu() | figure this out
             bbox_results.append(bbox_result)
 
+        # note that they work with deltas
+        trajectory = bbox_result["ego_fut_preds"][input.command].cumsum(dim=-2).numpy()
+
         return VADInferenceOutput(
-            trajectory=torch.zeros(6, 2).numpy(), aux_outputs=None
+            trajectory=trajectory,
+            aux_outputs=VADAuxOutputs(
+                objects_in_bev=bbox_results[0]["boxes_3d"].bevtolist(),
+                object_scores=bbox_results[0]["scores_3d"].tolist(),
+                object_classes=[self.classes[i] for i in bbox_results[0]["labels_3d"]],
+                segmentation=None,  # bev_h, bev_w
+                seg_grid_centers=None,  # bev_h, bev_w, 2 [x, y]
+            ),
         )
 
 
@@ -365,9 +325,10 @@ def _get_sample_input(nusc, nusc_can, scene_name, sample) -> VADInferenceInput:
 
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
+
     runner = VADRunner(
-        config_path="/UniAD/projects/configs/stage2_e2e/inference_e2e.py",
-        checkpoint_path="/UniAD/ckpts/uniad_base_e2e.pth",
+        config_path="/VAD/projects/configs/VAD/VAD_inference.py",
+        checkpoint_path="/VAD/ckpts/VAD_base.pth",
         device=torch.device(device),
     )
 
@@ -394,11 +355,6 @@ if __name__ == "__main__":
         fig, ax = plt.subplots(1, 2)
         ax[0].imshow(inference_input.imgs[0])
         ax[0].axis("off")
-
-        ax[1].plot(plan.trajectory[:, 0], plan.trajectory[:, 1], "r-*")
-        ax[1].set_aspect("equal")
-        ax[1].set_xlabel("x (m)")
-        ax[1].set_ylabel("y (m)")
 
         # save fig
         fig.savefig(f"{scene_name}_{str(i).zfill(3)}_{sample['timestamp']}.png")
