877041233ccb44a093d133d9a6abd6894fad18cf
Adam Tonderski <adam.tonderski@zenseact.com>
2024-03-07 10:07:55 +0100

    Freeze initial speed in baseline

--- inference/baseline.py
+++ inference/baseline.py
@@ -5,7 +5,7 @@ from inference.runner import VADInferenceInput, VADInferenceOutput, VADRunner
 
 CORRIDOR_WIDTH = 5  # meters
 CORRIDOR_LENGTH = 10  # meters
-CORRIDOR_START = 1  # meters
+CORRIDOR_START = 2.5  # meters
 TTC = 2  # seconds
 
 
@@ -14,17 +14,20 @@ class BaselineVADRunner(VADRunner):
         super().reset()
         self.corridor_length = None
         self.stop_updating_corridor = False
+        self.speed = None
 
     @torch.no_grad()
     def forward_inference(self, input: VADInferenceInput) -> VADInferenceOutput:
         output = super().forward_inference(input)
-        ego_vel = input.can_bus_signals[13]  # m/s
+        if self.speed is None:
+            self.speed = input.can_bus_signals[13]  # m/s
+        target_speed = self.speed
         if len(output.aux_outputs.objects_in_bev) != 0:
             objects = np.array(
                 output.aux_outputs.objects_in_bev
             )  # N x 5 (x, y, w, l, yaw) (y-forward, x-right)
             if not self.stop_updating_corridor:
-                self.corridor_length = ego_vel * TTC
+                self.corridor_length = target_speed * TTC
 
             objects_in_corridor_long = np.logical_and(
                 objects[:, 1] <= CORRIDOR_START + self.corridor_length,
@@ -34,17 +37,13 @@ class BaselineVADRunner(VADRunner):
                 objects_in_corridor_long,
                 np.abs(objects[:, 0]) <= CORRIDOR_WIDTH / 2,
             )
-            if not np.any(objects_in_corridor):
-                cur_vel = ego_vel
-            else:
+            if np.any(objects_in_corridor):
                 # break
-                cur_vel = 0.0  # m/s, set to low value, but not 0
+                target_speed = 0.0  # m/s, set to low value, but not 0
                 self.stop_updating_corridor = True
-        else:
-            cur_vel = ego_vel
 
         trajectory = np.zeros((6, 2))
-        trajectory[:, 1] = np.arange(1, 7) * cur_vel / 2  # y-forward
+        trajectory[:, 1] = np.arange(1, 7) * target_speed / 2  # y-forward
 
         return VADInferenceOutput(
             trajectory=trajectory,
